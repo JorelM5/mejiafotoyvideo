db3affa88e02b092f84daa1bbd3733abf7cef03b
JorelM5 <admin@dchaleco.com>
2025-07-01 11:38:46 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
-const CACHE_NAME = 'mejia-cache-v2';
+const CACHE_NAME = 'mejia-cache-v3';
 const urlsToCache = [
   '/',
   '/index.html',
   '/css/style.css',
-  // Solo incluye rutas que existan
+  '/img/logo.png',
+  'https://i.imgur.com/DjRBE3g.png' // Favicon externo
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache).catch(error => {
-          console.error('Error al cachear:', error);
-        });
-      })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
